7e462b5bca47499829ece721b9453b705e6007b2
Benny Zlotnik <uber442@gmail.com>
2015-07-18 17:16:30 +0300

    Display error when attempting to add a post with an existing URL

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -12,6 +12,10 @@ Template.postSubmit.events({
         return alert(error.reason);
       }
 
+      if (result.postExists) {
+        return alert('This link has already been posted');
+      }
+
       Router.go('postPage', post);
     });
   }
