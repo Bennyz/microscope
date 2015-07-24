e64724d672aadb0fce82104a60f958e8fd84411e
Benny Zlotnik <uber442@gmail.com>
2015-07-24 14:03:10 +0300

    Add disabled state after upvoting

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -7,6 +7,16 @@ Template.postItem.helpers({
 
   ownPost: function() {
     return this.userId === Meteor.userId();
+  },
+
+  upvotedClass: function() {
+    var userId = Meteor.userId();
+
+    if (userId && !_.include(this.upvoters, userId)) {
+      return 'btn-primary upvotable';
+    } else {
+      return 'disabled';
+    }
   }
 });
 
